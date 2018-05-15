03ed689c27b4b0dca854c85f3a646bc3c1f5f815
Pooja Kodavanti <pkodavan@gmail.com>
2018-05-14 17:08:35 -0700

    Add env vars and optimize Redis

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
 require('newrelic');
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const logger = require('./logger');
 const path = require('path');
 const bodyParser = require('body-parser');
-// var requestImg = require('request').defaults({ encoding: null });
-
-// const db = require('../database/mongo_db.js');
-const db = require('../database/postgres.js');
+const cors = require('cors');
+const compression = require('compression');
+const db = require('./postgres_redis.js');
 
 const app = express();
-const PORT = 3002;
+const PORT = process.env.EXP_PORT || 3002;
 
 app.use(morgan('dev', {
   skip(req, res) {
@@ -26,62 +26,37 @@ app.use(morgan('dev', {
   stream: process.stdout,
 }));
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
+app.use(cors());
+app.use(compression());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/restaurants/', express.static(`${__dirname}/../client/dist`));
-app.use((req, res, next) => {
-  logger.error('404 page requested');
-  res.status(404).send('This page does not exist!');
-});
-
-
-app.get('/restaurants/:id', (req, res) => {
+const logStats = () => {
   logger.debug('Debug statement');
   logger.info('Info statement');
+};
+
+app.use('/restaurants/', express.static(`${__dirname}/../client/dist`));
+
+app.get('/restaurants/:id', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../client/dist/index.html`));
 });
 
 app.get('/api/restaurants/:id/gallery', (req, res) => {
-  db.findOne(parseInt(req.params.id, 10))
-    .then((place) => {
-      const s3String = 'https://s3-us-west-2.amazonaws.com/apateez-photos/';
-      const restaurantPhotosArray = [];
-      const restaurant = place[0];
-      for (let i = 0; i < restaurant.photos.length; i += 1) {
-        restaurantPhotosArray.push(`${s3String}${restaurant.photos[i]}.png`);
-      }
-      logger.debug('Debug statement');
-      logger.info('Info statement');
-      res.send({ photoArray: restaurantPhotosArray, restaurantName: restaurant.name, place_id: restaurant.place_id });
-    })
-    .catch(err => console.log(err));
+  logStats();
+  db.getFromCache(req, res);
 });
 
 app.get('/:searchValue', (req, res) => {
-  const searchQuery = req.params.searchValue;
-  const recursefindPlaceId = function (searchQuery) {
-    const query = Places.findOne({ name: { $regex: searchQuery, $options: 'i' } });
-    query.exec((err, photos) => {
-      if (err) {
-        console.log(err);
-      } else if (photos) {
-        res.send({ place_id: photos.place_id });
-      } else {
-        searchQuery = searchQuery.slice(0, -1);
-        recursefindPlaceId(searchQuery);
-      }
-    });
-  };
-  recursefindPlaceId(searchQuery);
+  logStats();
+  db.search(req, res);
 });
 
+app.use((req, res) => {
+  logger.error('404 page requested');
+  res.status(404).send('This page does not exist!');
+});
 
 app.listen(PORT, () => {
   logger.info(`listening on port ${PORT}`);
 });
+
---
+++ server/postgres_redis.js
@@ -0,0 +1,70 @@
+const promise = require('bluebird');
+require('dotenv').config();
+
+const initOptions = {
+  promiseLib: promise,
+};
+const pgp = require('pg-promise')(initOptions);
+
+const cn = {
+  host: 'localhost',
+  port: 5432,
+  database: 'apateez',
+};
+const db = pgp(cn);
+const redis = require('redis');
+
+const REDIS_PORT = process.env.REDIS_PORT || 6379;
+const client = redis.createClient(REDIS_PORT);
+
+const getFromDB = (req, res) => {
+  db.any('SELECT * FROM gallery WHERE place_id = $1', req.params.id)
+    .then((place) => {
+      const s3String = 'https://s3-us-west-2.amazonaws.com/apateez-photos/';
+      const photoUrls = [];
+      const restaurant = place[0];
+      for (let i = 0; i < restaurant.photos.length; i += 1) {
+        photoUrls.push(`${s3String}${restaurant.photos[i]}.png`);
+      }
+      const obj = {
+        photoArray: photoUrls,
+        restaurantName: restaurant.name,
+        place_id: restaurant.place_id,
+      };
+      client.setex(req.params.id, 60 * 60, JSON.stringify(obj));
+      res.send(obj);
+    })
+    .catch(err => console.log(err));
+};
+
+const getFromCache = (req, res) => {
+  client.get(req.params.id, (err, place) => {
+    if (place) {
+      res.send(place);
+    } else {
+      getFromDB(req, res);
+    }
+  });
+};
+
+const search = (req, res) => {
+  const recursefindPlaceId = (q) => {
+    db.any('SELECT * FROM gallery WHERE gallery.name=$1', q)
+      .then((place) => {
+        if (place) {
+          res.send({ place_id: place.place_id });
+        } else {
+          recursefindPlaceId(q.slice(0, -1));
+        }
+      })
+      .catch(err => console.log(err));
+  };
+  recursefindPlaceId(req.params.searchValue);
+};
+
+
+module.exports = {
+  search,
+  getFromDB,
+  getFromCache,
+};
